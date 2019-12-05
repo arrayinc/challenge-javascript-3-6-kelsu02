e35659f6b88b87cdeff3d09c6f476f5bcebd00cb
kredmond <kelsu02@gmail.com>
2019-12-05 16:08:08 -0700

    it works, not that pretty

--- answer.js
+++ answer.js
@@ -1,1 +1,18 @@
 // *** YOUR ANSWER BELOW ***
+function matchNumber(N) {
+    let fib1 = 1;
+    let fib2 = 1;
+    if (N <= 2 ) {
+        fib1 = 1;
+    } else {
+        for (let i = 3; i <= N; i++) {
+            let fib3 = fib1 + fib2;
+            fib2 = fib1;
+            fib1 = fib3;
+            let result = fib3;
+    }
+    }
+    console.log (`${fib1} in number ${N} in the Fibonacci sequence`);
+}
+
+matchNumber(13);
