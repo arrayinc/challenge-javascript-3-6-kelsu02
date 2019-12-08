b431e4112bd8206de6acccf3c2490f8ad95a8aa8
kredmond <kelsu02@gmail.com>
2019-12-07 19:40:02 -0700

    Still haven't figured out how to start without skipping a number but cleaned it up a little.

--- answer.js
+++ answer.js
@@ -1,18 +1,17 @@
 // *** YOUR ANSWER BELOW ***
 function matchNumber(N) {
     let fib1 = 1;
-    let fib2 = 1;
-    if (N <= 2 ) {
+    let fib2 = 0;
+    if (N <= 1 ) {
         fib1 = 1;
     } else {
         for (let i = 3; i <= N; i++) {
             let fib3 = fib1 + fib2;
             fib2 = fib1;
             fib1 = fib3;
-            let result = fib3;
     }
     }
-    console.log (`${fib1} in number ${N} in the Fibonacci sequence`);
+    console.log (`${fib1} is number ${N} in the Fibonacci sequence`);
 }
 
-matchNumber(13);
+matchNumber(2);
